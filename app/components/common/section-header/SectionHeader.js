8b852575d6e79ced39112f4d930f33279c4e7f7e
superdorkman <276382355@qq.com>
2018-12-21 10:20:18 +0800

    add bg

---
+++ app/components/common/section-header/SectionHeader.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export class SectionHeader extends Component {
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SectionHeader)
--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../components/common/private/PrivateRoute';
+import styled from 'styled-components';
 
 import Sidebar from '../components/sidebar/Sidebar';
 import Center from '../components/center/Center';
@@ -10,28 +11,39 @@ import Shouhuo from '../components/shouhuo/Shouhuo';
 import Orders from '../components/orders/Orders';
 import Warehouse from '../components/warehouse/Warehouse';
 
+import bg from '../assets/images/bg.png';
 
 class Home extends Component {
 
   render() {
     const { match } = this.props;
-    console.log(`${match.url}/`)
+
     return (
       <Fragment>
         <Sidebar />
-        <Switch>
-          <Route exact path={`${match.url}/`} component={Center} />
-          <Route exact path='/finance' component={Finance} />
-          <Route exact path='/chuhuo' component={Chuhuo} />
-          <Route exact path='/orders' component={Orders} />
-          <Route exact path='/shouhuo' component={Shouhuo} />
-          <Route exact path='/warehouse' component={Warehouse} />
-        </Switch>
+        <Container>
+          <Switch>
+            <Route exact path={`${match.url}/`} component={Center} />
+            <Route exact path='/finance' component={Finance} />
+            <Route exact path='/chuhuo' component={Chuhuo} />
+            <Route exact path='/orders' component={Orders} />
+            <Route exact path='/shouhuo' component={Shouhuo} />
+            <Route exact path='/warehouse' component={Warehouse} />
+          </Switch>
+        </Container>
       </Fragment>
     );
 
   }
 }
 
+const Container = styled.div`
+  padding: 20px;
+  flex: 1;
+  background-image: url(${bg});
+  background-color: #fff;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+`;
 
 export default Home;
